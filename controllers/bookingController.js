e0ad303f9109794373bc6ad4295c993315aece0a
Jatin Madaan <sjmadaan143@iiitsonepat.ac.in>
2024-04-08 00:49:56 +0530

    Stripe webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,8 +52,8 @@ const createBooking = async (session) => {
   const tour = session.client_reference_id;
   const user = await User.findOne({ email: session.customer_email });
   const price = session.amount_total / 100;
-  console.log(user);
-  const x = user.id;
+  //console.log(user);
+  const x = user._id;
   //if (!tour || !user || !price) return next();
   await Booking.create({ tour, x, price });
   // res.redirect(req.originalUrl.split('?')[0]);
